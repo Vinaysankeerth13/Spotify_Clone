160a7f6cc594cec0e14701ff0ee9ce5fa21bf45e
Vinaysankeerth13 <vinaysankeerth1@gmail.com>
2024-04-22 20:23:44 +0530

    Prev and Next functionality

--- Spotify_Clone/script.js
+++ Spotify_Clone/script.js
@@ -1,120 +1,148 @@
-
 let currentSong = new Audio();
 
-async function getSongs(){
-    let a = await fetch("http://127.0.0.1:3000/Spotify_Clone/Songs/")
-    let response = await a.text()
-    let div = document.createElement("div")
-    div.innerHTML = response
-    let as = div.getElementsByTagName("a")
-    let songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if(element.href.endsWith(".mp3"))
-        {
-            songs.push(element.href.split(`/Songs/`)[1])
-        }
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:3000/Spotify_Clone/Songs/");
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split(`/Songs/`)[1]);
     }
-    return songs
+  }
+  return songs;
 }
 
 const playMusic = (track, pause = false) => {
-    // Check if track already includes file extension
-    if (!track.endsWith(".mp3")) {
-        track += ".mp3"; // Append file extension if necessary
-    }
-    currentSong.src = "/Spotify_Clone/Songs/" + track;
-    if (!pause) {
-        currentSong.play();
-        play.src = "Images/pause1.svg";
-    }
-    document.querySelector(".songinfo").innerHTML = decodeURI(track);
-    document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+  // Check if track already includes file extension
+  if (!track.endsWith(".mp3")) {
+    track += ".mp3"; // Append file extension if necessary
+  }
+  currentSong.src = "/Spotify_Clone/Songs/" + track;
+  if (!pause) {
+    currentSong.play();
+    play.src = "Images/pause1.svg";
+  }
+  document.querySelector(".songinfo").innerHTML = decodeURI(track);
+  document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
 };
 
-
 function secondsToMinutesSeconds(seconds) {
-    if (isNaN(seconds) || seconds < 0) {
-         return "00:00";
-    }
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-     return `${formattedMinutes}:${formattedSeconds}`;
+  if (isNaN(seconds) || seconds < 0) {
+    return "00:00";
+  }
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+  return `${formattedMinutes}:${formattedSeconds}`;
 }
 
+async function main() {
+  // Gets the list of songs
+  let songs = await getSongs();
+  playMusic(songs[0], true);
 
-// http://127.0.0.1:3000/Spotify_Clone/Songs/Adavi%20Gusagusalu.mp3
-// http://127.0.0.1:3000/Spotify_Clone/Adavi%20Gusagusalu.mp3
-
-
-async function main(){
-
-    // Gets the list of songs
-    let songs = await getSongs()
-    playMusic(songs[0], true)
-
-    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-    songUL.innerHTML = ""
-    for (const song of songs) {
-        let formattedSong = song;
-        if (typeof song !== 'string') {
-            formattedSong = song.toString();
-        }
-        formattedSong = formattedSong.replaceAll("%20", " ");
-        formattedSong = formattedSong.replaceAll(".mp3", "");
-        songUL.innerHTML = songUL.innerHTML + `<li>
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  songUL.innerHTML = "";
+  for (const song of songs) {
+    let formattedSong = song;
+    if (typeof song !== "string") {
+      formattedSong = song.toString();
+    }
+    formattedSong = formattedSong.replaceAll("%20", " ");
+    formattedSong = formattedSong.replaceAll(".mp3", "");
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `<li>
             <img  class="invert" width="34" src="Images/music.svg" alt="">
             <div class="info">
             <div>${formattedSong}</div> 
             </div>
             <img class="invert" src="Images/play.svg" alt="">
         </li>`;
+  }
+
+  // Attach an event listener to each song
+  Array.from(
+    document.querySelector(".songList").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    });
+  });
+
+  // Attach event listener to play,next and previous
+  play.addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play();
+      play.src = "Images/pause1.svg";
+    } else {
+      currentSong.pause();
+      play.src = "Images/play.svg";
     }
+  });
 
-    // Attach an event listener to each song
-    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
-        e.addEventListener("click", element=>{
-            console.log(e.querySelector(".info").firstElementChild.innerHTML)
-            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
-        })
-    })
+  // Listen for time update event
+  currentSong.addEventListener("timeupdate", () => {
+    document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(
+      currentSong.currentTime
+    )} / ${secondsToMinutesSeconds(currentSong.duration)}`;
+    document.querySelector(".circle").style.left =
+      (currentSong.currentTime / currentSong.duration) * 100 + "%";
+  });
 
-    // Attach event listener to play,next and previous
-    play.addEventListener("click", ()=> {
-        if(currentSong.paused){
-            currentSong.play()
-            play.src = "Images/pause1.svg" 
-        }
-        else{
-            currentSong.pause()
-            play.src = "Images/play.svg"
-            
-        }
-    })
+  // Add an eventlistener to seekbar
+  document.querySelector(".seekbar").addEventListener("click", (e) => {
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".circle").style.left = percent + "%";
+    currentSong.currentTime = (currentSong.duration * percent) / 100;
+  });
 
-    // Listen for time update event
-    currentSong.addEventListener("timeupdate", () => {
-        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
-        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
-        })
+  // Add eventlistener to opening the hamburger
+  document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".left").style.left = "0";
+  });
 
-    // Add an eventlistener to seekbar 
-    document.querySelector(".seekbar").addEventListener("click", e => {
-        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
-        document.querySelector(".circle").style.left = percent + "%";
-        currentSong.currentTime = ((currentSong.duration) * percent) / 100
-        })
+  // Add eventlistener to closing the hamburger
+  document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".left").style.left = "-120%";
+  });
+
+  // Add an event listener to previous
+  previous.addEventListener("click", () => {
+    currentSong.pause();
+    console.log("Previous clicked");
+
+    let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
+    if (index - 1 >= 0) {
+      playMusic(songs[index - 1]);
+    }
+  });
+
+  // Add an event listener to next
+  next.addEventListener("click", () => {
+    currentSong.pause();
+    console.log("Next clicked");
+
+    let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
+    if (index + 1 < songs.length) {
+      playMusic(songs[index + 1]);
+    }
+  });
 }
-
-main();
+main(); 
 
 // // console.log('Lets write JavaScript');
-// // let currentSong = new Audio();
+// // let currentSong = new Audio(); ̰
 // // let songs;
 // // let currFolder;
-
 
 // async function getSongs(folder) {
 //     currFolder = folder;
